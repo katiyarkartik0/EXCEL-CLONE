15320ca03543e8a15fc317bb3ac439288fa1647d
katiyarkartik0 <katiyarkartik0@gmail.com>
2021-07-31 01:22:43 +0530

    functionality added 1

--- styleScript.js
+++ styleScript.js
@@ -4,6 +4,7 @@ let leftAlign = allAlignmentOptions[0];
 let centerAlign = allAlignmentOptions[1];
 let rightAlign = allAlignmentOptions[2];
 
+
 leftAlign.addEventListener("click", function(e){
     if(lastCell){
         lastCell.style.textAlign = "left";
@@ -15,7 +16,7 @@ centerAlign.addEventListener("click", function(e){
     if(lastCell){
         lastCell.style.textAlign = "center";
         let address = lastCell.getAttribute("data-address");
-        dataObj[address].align = "right";
+        dataObj[address].align = "center";
     }
 })
 rightAlign.addEventListener("click", function(e){
@@ -32,9 +33,11 @@ let bold = allBoldItalicUnderlineOptions[0];
 let italics = allBoldItalicUnderlineOptions[1];
 let underline = allBoldItalicUnderlineOptions[2];
 
-bold.addEventListener("click", function(e){
+bold.addEventListener("click", function(e){ 
     if(lastCell){
-        lastCell.style.fontWeight = "bold";
+        lastCell.style.fontWeight = "900";
+        let address = lastCell.getAttribute("data-address");
+        dataObj[address].isBold = "true";
     }
 })
 italics.addEventListener("click", function(e){
@@ -47,4 +50,39 @@ underline.addEventListener("click", function(e){
         lastCell.style.textDecoration = "underline"
     }
 })
+
+let allCellColorOptions = document.querySelectorAll(".cell-color-options span");
+let body = document.querySelector("body")
+
+let bgColorPicker = allCellColorOptions[0];
+bgColorPicker.addEventListener("click", function(e){
+    let colorPickerElement = document.createElement("input");
+    colorPickerElement.type = "color";
+    body.append(colorPickerElement);
+    colorPickerElement.click();
+    colorPickerElement.addEventListener("input", function(e){
+        if(lastCell){
+            lastCell.style.backgroundColor = e.currentTarget.value;
+            let address = lastCell.getAttribute("data-address");
+            dataObj[address].bgColor = e.currentTarget.value;
+        }
+    });
+});
+
+let fontColorPicker = allCellColorOptions[1];
+fontColorPicker.addEventListener("click", function(e){
+    let colorPickerElement = document.createElement("input");
+    colorPickerElement.type = "color";
+    body.append(colorPickerElement);
+    colorPickerElement.click();
+    colorPickerElement.addEventListener("input", function(e){
+        if(lastCell){
+            lastCell.style.color = e.currentTarget.value;
+            let address = lastCell.getAttribute("data-address");
+            dataObj[address].Color = e.currentTarget.value;
+        }
+    });
+});
+
+
 
